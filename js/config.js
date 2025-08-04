cd0850aee3b98c46f32391ae42b70763930009e7
ryzx <rezzzyxz@gmail.com>
2025-08-04 07:15:37 +0700

    Add config.js with initial configuration

---
+++ js/config.js
@@ -0,0 +1,55 @@
+const CONFIG = {
+    name: 'Rezzxzx',
+    title: 'Full Stack Developer',
+    email: 'rezzzyxz@gmail.com',
+    github: 'https://github.com/rezzxzx',
+    socials: {
+        github: 'rezzxzx',
+        instagram: 'rezzxzx_',
+        twitter: 'rezzxzx_'
+    },
+    skills: [
+        {
+            name: 'JavaScript',
+            level: 90
+        },
+        {
+            name: 'HTML/CSS',
+            level: 95
+        },
+        {
+            name: 'React.js',
+            level: 85
+        },
+        {
+            name: 'Node.js',
+            level: 80
+        },
+        {
+            name: 'Python',
+            level: 75
+        },
+        {
+            name: 'SQL',
+            level: 85
+        }
+    ],
+    projects: [
+        {
+            title: 'Portfolio Website',
+            description: 'Modern portfolio website with dark theme and music player',
+            image: 'images/portfolio.jpg',
+            github: 'https://github.com/rezzxzx/portofolioo'
+        }
+    ],
+    music: {
+        defaultPlaylist: [
+            {
+                title: 'Lofi Study',
+                artist: 'Lofi Girl',
+                cover: 'images/music/lofi-study.jpg',
+                file: 'music/lofi-study.mp3'
+            }
+        ]
+    }
+};
